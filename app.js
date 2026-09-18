ac1010ebd6eb9ad8fd463f62505dd5e493a2c287
Andresccc <00054525@uca.edu.sv>
2026-09-18 00:25:49 -0600

    :sparkles: feat: comentarios para app.js y correcciones

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+//importar el express
 const express = require('express');
+const app = express();
+//importar las rutas y el controlador de incidencias
 const incidenciasRoutes = require("./routes/incidencias");
 const incidenciasController = require("./controllers/incidenciasController");
-const app = express();
+//el puerto donde se ejecutara el servidor
 const port = 3124;
-
+//express entienda archivos json
 app.use(express.json());
-
+//definir las rutas para incidencias y estadisticas
 app.use("/incidencias", incidenciasRoutes);
 app.get("/estadisticas", incidenciasController.getEstadisticas);
-
+//iniciar el servidor en el puerto asignado y mostrar un mensaje en la terminal
+// que te diga que el servidor esta corriendo y en que puerto
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 }); 
