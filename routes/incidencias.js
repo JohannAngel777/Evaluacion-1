9d8e5fa001f3d6fd694a5b37d2f4b0fd4239ed8b
Johann Angel <00071625@uca.edu.sv>
2026-09-18 01:02:28 -0600

    docs: Comentarios para helpers.js y incidencias.js

--- routes/incidencias.js
+++ routes/incidencias.js
@@ -1,7 +1,9 @@
+// Router almacena todas las rutas de incidencias
 const express = require('express');
 const router = express.Router();
 const incidenciasController = require('../controllers/incidenciasController');
 
+// Endpoints de incidencias
 router.post('/', incidenciasController.crearIncidencia);
 router.get('/', incidenciasController.listarIncidencias);
 router.get('/:id', incidenciasController.buscarIncidenciaPorId);
@@ -9,4 +11,5 @@ router.put('/:id/estado', incidenciasController.cambiarEstado);
 router.delete('/:id', incidenciasController.eliminarIncidencia);
 router.get('/:id/clasificacion', incidenciasController.getClasificacion);
 
+// Exporta las rutas hacia app.js
 module.exports = router;
